ce724ccb62f205a07c4799087982a08ba3e6231a
Fan Young <59141023+fan19-hub@users.noreply.github.com>
2021-07-14 15:12:36 +0800

    Network check and connect

--- autostart.bat
+++ autostart.bat
@@ -1,2 +1,8 @@
-ping localhost -n 4
-python F:\OneDrive - International Campus, Zhejiang University\Fan Yang\我的\ZJU-AutoHitProject-master\ZJU-AutoHitProject-master\AutoHit.py
+netsh wlan connect name=ZJUWLAN
+::line 1 meant to connect the WIFI ZJUWLAN
+
+cd C:\Users\fan.19\fan19-hub\ZJU-auto-punch
+::line 2 meant to enter the directory where AutoHit.py is
+
+python AutoHit.py
+::run it
