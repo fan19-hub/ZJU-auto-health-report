8b90c56cf960199e4eca654f9f87203a1a0a7db0
Fan Young <59141023+fan19-hub@users.noreply.github.com>
2021-07-27 11:40:00 +0800

    增加了重试时，网页可见的功能

--- AutoHit.py
+++ AutoHit.py
@@ -1,8 +1,6 @@
-
 """"
-@Author：Tishacy
-reference: https://github.com/Tishacy/ZJU-nCov-Hitcarder
-@Modified: fan19-hub
+@Reference: https://github.com/Tishacy/ZJU-nCov-Hitcarder
+@Author: fan19-hub
 Date:7/14/2021
 """
 
@@ -11,11 +9,13 @@ from selenium.webdriver.chrome.options import Options
 import json
 import os
 from time import sleep
+import math
 
 SUCCESS=1
 FAILURE=0
 username=""
 password=""
+visible=0
 
 def check_network():
       print("\n\n正在检查网络连接：")
@@ -60,9 +60,11 @@ def get_your_info():
 
 
 def openPage(url):
+      global visible
       """ 打开网页 """
       opt = Options()
-      opt.add_argument('--headless')
+      if not visible:
+            opt.add_argument('--headless')
       opt.add_argument('--disable-gpu')
       try:
             driver_path=r".\chromedriver.exe"
@@ -102,21 +104,13 @@ def login(browser,username, password)->bool:
             return SUCCESS
       
 
-def submit(browser)->bool:
+def submit(browser)->bool:      
       """ 提交健康打卡 """
+      global visible
       #是否在校
       item1 = browser.find_element_by_xpath("//div[@name='sfzx']/div/div[2]")
       item1.click()
-      try:
-            #地区
-            addrs = browser.find_element_by_xpath("//div[@name='area']/input")
-            addrs.click()
-            error =browser.find_element_by_xpath("//div[@class='wapat-inner']/div[@class='wapat-title']")
-            print(error.text,"，请开启位置权限再重试，打卡失败")
-            return FAILURE
-      
-      except:
-            print("获取位置信息成功")
+    
 
       #本人家庭成员(包括其他密切接触人员)是否有近14日入境或近14日拟入境的情况
       item2 = browser.find_element_by_xpath("//div[@name='sfymqjczrj']/div/div[2]")
@@ -133,39 +127,61 @@ def submit(browser)->bool:
       button1.click()
 
       try:
+            #地区
+            addrs = browser.find_element_by_xpath("//div[@name='area']/input")
+            addrs.click()
+            error =browser.find_element_by_xpath("//div[@class='wapat-inner']/div[@class='wapat-title']")
+            print(error.text,"，获取位置信息失败，正在为您打开网页，请手动设置")
+            visible=1
+            return FAILURE
+      
+      except:
+            print("获取位置信息成功")
+
+      try:
             button2 = browser.find_element_by_xpath("//div[@class='wapcf-btn-box']/div[2]")
             button2.click()
       except:
             a=browser.find_element_by_xpath("//div[@class='wapat-inner']")
             if "每天只能填报一次" in a.text:
                   print("每天只能填报一次，您已提交过")
+                  print("打卡完成")
+                  return SUCCESS
             else:
-                  print("未弹出确认提交对话框，可能因为没有缓存导致默认选项未勾选，建议先手动提交一次")
                   print(a.text)
-            print("打卡失败~")
-            return FAILURE
+                  print("有些选项需要您手动勾选,正在为您打开网页")
+                  visible=1
+                  return FAILURE
       print("打卡成功！")
       return SUCCESS
-     
+
+def retry_setting():
+      print("您可以：\n\t按回车键重试打卡\n\t或关闭窗口退出\n\t或输入C更改学号和密码，并自动重试")
+      command=input(":")
+      if "C" in command or "c" in command:            #if you want to change the ID or password
+            open("userdata.json", 'w').close()
+            get_your_info()
+def main():
+      global visible
+
+      check_network()
+
+      get_your_info()
+
+      for count in range(3):                  
+            bs=openPage("https://healthreport.zju.edu.cn/ncov/wap/default/index")   
+
+            if bs==None or FAILURE==login(bs, username, password):      #if opening page or loging failed
+                  retry_setting()
+            else:                                  #if opening page and login succeed
+                  res=submit(bs)                #submit the page
+                  if visible==1:
+                        input()
+                  bs.close()               #close it
+                  if res==SUCCESS:
+                        break
 
 
 if __name__ == "__main__":
-      check_network()
-      get_your_info()
-      
-      bs=openPage("https://healthreport.zju.edu.cn/ncov/wap/default/index")
-      res=FAILURE
-      if bs!=None and SUCCESS==login(bs, username, password): #if opening page and loging in successfully
-            res=submit(bs) #submit the page
-            bs.close() #close it
-      if res==FAILURE:
-            print("\n\n手动打卡网址：https://healthreport.zju.edu.cn/ncov/wap/default/index")
-            print("您可以：\n\t按回车键重试打卡\n\t或关闭窗口退出\n\t或输入C更改学号和密码，并自动重试")
-            command=input(":")
-            if "C" in command or "c" in command:
-                  open("userdata.json", 'w').close()
-                  get_your_info()
-            try:
-                  os.system("start "+os.getcwd()+"\\AutoHit.exe")
-            except:
-                  os.system("python AutoHit.py")
+      main()
+
