26135d2b91a41ad61144bf28bf74dc850d00aaee
Fan Young <59141023+fan19-hub@users.noreply.github.com>
2021-08-12 13:48:10 +0800

    Delete autostart.bat

--- autostart.bat
+++
@@ -1,7 +0,0 @@
-netsh wlan connect name=ZJUWLAN
-::line 1 meant to connect the WIFI ZJUWLAN
-cd C:\Users\fan.19\fan19-hub\ZJU-auto-punch
-::line 2 meant to enter the directory where AutoHit.py is
-python AutoHit.py
-::run it
-pause
